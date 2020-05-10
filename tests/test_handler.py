162ec5fe9042962d603ccb67a7b5aeb0fb76c918
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-05-10 15:26:13 +0100

    Other Settings Changes

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,16 +1,40 @@
 import unittest
-import index
+from index import handler
+import hello
 
 
 class TestHandlerCase(unittest.TestCase):
 
     def test_response(self):
         print("testing response.")
-        result = index.handler(None, None)
-        print(result)
+        result = handler(None, None)
+        # print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
         self.assertIn('Hello World', result['body'])
+
+    def test_hello_handler(self):
+
+        # Arrange
+        event = {
+            'pathParameters': {
+                'name': 'testname'
+            }
+        }
+        context = {}
+        expected = {
+            'body': '{"output": "Hello testname"}',
+            'headers': {
+                'Content-Type': 'application/json'
+            },
+            'statusCode': 200
+        }
+
+        # Act
+        result = hello.handler(event, context)
+
+        # Assert
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
